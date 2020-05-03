99a5a1492740df41f51e60fa66e230827e1c7817
cangyin <1522645226@qq.com>
2020-05-03 16:02:05 +0800

    fix build“

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="coloredlog",
-    version="0.2.1",
+    version="0.2.5",
     author="CangYin",
     author_email="excangyin@gmail.com",
     description="Colorize your console output.",
